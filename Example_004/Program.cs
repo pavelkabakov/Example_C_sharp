2e6bdbc24bc5f1b275d2818de526c11bbdd4cae7
Павел Кабаков <pavelkabakov@gmail.com>
2023-03-21 22:06:01 +0300

    add random 2

--- Example_004/Program.cs
+++ Example_004/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+double number_a = 12;
+double number_b = 5;
+double result = number_a / number_b;
+Console.WriteLine(result);
+int random = new Random().Next(1,10);
+Console.WriteLine(random);
