41ee3760b8146f6671ccd514c87d82c7958de801
Павел Кабаков <pavelkabakov@gmail.com>
2023-03-25 10:45:54 +0300

    add seminar_001

---
+++ Seminar_001/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Вводим число
+преобразуем строку в число
+выводим число
+выводим квадрат числа
+*/
+
+Console.Write("Введите число - ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+Console.WriteLine("Вы ввели - ");
+Console.WriteLine(number);
+Console.WriteLine("Квадрат числа = ");
+Console.WriteLine(number*number);
