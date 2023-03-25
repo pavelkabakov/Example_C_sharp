0cfc5b60cef3c60ad876fc2ea6ae9d56ef5c93cb
Павел Кабаков <pavelkabakov@gmail.com>
2023-03-25 11:27:35 +0300

    seminar_003 final

--- Seminar_003/Program.cs
+++ Seminar_003/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача No3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+20 минут
+*/
+
+// Вводим число
+Console.Write("Введите число дня недели - ");
+string input1 = Console.ReadLine();
+
+if ((input1) == "1")
+{
+    Console.WriteLine("Сегодня Понедельник");
+}
+if ((input1) == "2")
+{
+    Console.WriteLine("Сегодня Вторник");
+}
+if ((input1) == "3")
+{
+    Console.WriteLine("Сегодня Среда");
+}
+if ((input1) == "4")
+{
+    Console.WriteLine("Сегодня Четверг");
+}
+if ((input1) == "5")
+{
+    Console.WriteLine("Сегодня Пятница");
+}
+if ((input1) == "6")
+{
+    Console.WriteLine("Сегодня Суббота");
+}
+if ((input1) == "7")
+{
+    Console.WriteLine("Сегодня Воскресенье");
+}
