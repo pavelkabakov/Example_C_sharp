b3a4afe1dcf138fd2efb5fcabf317be7ab55885f
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-01 11:06:26 +0300

    task_18

--- Seminar_013/Program.cs
+++ Seminar_013/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите x:");
+int x = int.Parse(Console.Readline());
+Console.WriteLine("Введите y:");
+int y = int.Parse(Console.Readline());
+
+int result = GetNumberOfQuarter (x, y);
+Console.WriteLine($"X and Y are in {result} quarter");
+
+int GetNumberOfQuarter (int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }    
+
+    if (y > 0 && x < 0)
+    {
+        return 2;
+    }    
+
+    if (x > 0 && y > 0)
+    {
+        return 3;
+    }    
+
+    if (x > 0 && y < 0)
+    {
+        return 4;
+    }     
+
+    return 0;
+}
+
+
---
+++ Seminar_014/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Задача No18. Общее обсуждение
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Задача No21. Работа в группах
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// ● A (3,6); B (2,1) -> 5,09
+// ● A (7,-5); B (1,-1) -> 7,21
+
+
+
+Console.WriteLine("Введите номер четверти");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine($"Вы ввели {x}");
+
+string range = RangeQuarter(x);
+Console.WriteLine($"Диапазон для {x} четверти - {range}");
+
+string RangeQuarter (int x)
+{
+    if (x == 1)
+    {
+        return range = "x>0 and y>0 ";
+    }
+    else if (x == 2)
+    {
+        return range = "x<0 and y>0 ";
+    }
+    else if (x == 3)
+    {
+        return range = "x<0 and y<0 ";
+    }
+    else if (x == 4)
+    {
+        return range = "x>0 and y<0 ";
+    }
+    return "неверное значение";
+}
