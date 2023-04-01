2eb827ad8c3830808bce25ea9981559b2e701156
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-01 20:01:43 +0300

    Добавил задачу по сортировке массива

--- Lesson_3_4/Program.cs
+++ Lesson_3_4/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Clear();
+
+int[] arr = { 1, 5, 4, 6, 7, 8, 66, 73, 2, 4, 4, 88, 687 };
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                  minPosition = j;
+            }
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+}
+
