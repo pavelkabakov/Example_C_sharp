9382c6b5aa70ebf81d8dfdfd2ca5884949f06128
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-02 11:21:53 +0300

    добавил задачу 30

--- Seminar_4_2/Program.cs
+++ Seminar_4_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24
+// напишите программу которая принимает на вход число А
+// и выдает сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+int sum = GetSumToNumber(a);
+Console.WriteLine(sum);
+
+int GetSumToNumber(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
--- Seminar_4_3/Program.cs
+++ Seminar_4_3/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 5 -> 120
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int number = 8; // задаем число символов
+int[] array = new int[number];
+
+Random random = new Random();
+
+    for (int i = 1; i < number; i++)
+    {
+        int randomValue = random.Next(0, 2);
+        array[i]= randomValue;
+    }
+
+PrintArray (array);
+
+void PrintArray (int[] array)
+{
+    for (int i = 1; i < number; i++)
+        {
+           Console.WriteLine(array[i]);
+        }
+}
+
