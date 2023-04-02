ac0901facc30cdab3c69f3cab6fb43c47a0fbb52
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-02 11:34:47 +0300

    Задача 30

--- Seminar_4_3/Program.cs
+++ Seminar_4_3/Program.cs
@@ -1,13 +1,15 @@
-﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
-// 4 -> 24 5 -> 120
-
-// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
 // заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-int number = 8; // задаем число символов
+int number = 8; // задаем длинну массива
 int[] array = new int[number];
 
+FillArrayRandom(number);
+PrintArray (array);
+
+int[] FillArrayRandom(int length)
+{
 Random random = new Random();
 
     for (int i = 1; i < number; i++)
@@ -15,8 +17,8 @@ Random random = new Random();
         int randomValue = random.Next(0, 2);
         array[i]= randomValue;
     }
-
-PrintArray (array);
+return array;
+}
 
 void PrintArray (int[] array)
 {
