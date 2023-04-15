8a7d99d59110d84eb0d7641d20fe982e5b95d918
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-15 12:11:29 +0300

    конец семинара

--- seminar_7_3_two_dim_array/Program.cs
+++ seminar_7_3_two_dim_array/Program.cs
@@ -1,6 +1,7 @@
-﻿// Задача 51: Задайте двумерный массив. Найдите элементы, у
-// которых оба индекса чётные, и замените эти элементы на их
-// квадраты.
+﻿// Задача 51: Задайте двумерный массив.
+// Найдите сумму 
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -8,33 +9,28 @@
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
 int[,] table = new int[3, 4];
-table[0, 0] = 1; table[0, 1] = 4; table[0, 2] = 7; table[0, 3] = 2;
-table[1, 0] = 5; table[1, 1] = 9; table[1, 2] = 2; table[1, 3] = 3;
-table[2, 0] = 8; table[2, 1] = 4; table[2, 2] = 2; table[2, 3] = 4;
-
 
 Console.Clear();
-// FillArray(table); // заполняем массив
+FillArray(table); // заполняем массив
 PrintArray(table); // выводим массив
-ChangeNumberArray(table);
 Console.WriteLine();
-PrintArray(table); // выводим массив
+Console.WriteLine(GetDiagonalSumm(table));
 
-void ChangeNumberArray (int[,] table) // заменяем ячейку с четными индексами на квадрат
+
+int GetDiagonalSumm (int[,] table) // ищем сумму диагонали
 {
+    int summDiagonal = 0;
     for (int rows = 0; rows < table.GetLength(0); rows++)
     {
-        if ( rows%2 == 0)
-        {
-            for (int columns = 0; columns < table.GetLength(1); columns++)
+        for (int columns = 0; columns < table.GetLength(1); columns++)
             {
-                if ( columns%2 == 0)
+                if ( rows == columns )
                 {
-                   table[rows, columns] = table[rows, columns] * table[rows, columns];
+                   summDiagonal += table[rows, columns];
                 }
             }
-        }
     }
+    return summDiagonal;
 }
 
 void PrintArray(int[,] tab) // вывод массива
