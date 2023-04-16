fda5b1ce7159543fb4dabf77f30fc215f8c199a2
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-16 10:33:53 +0300

    Задача 53: Задайте двумерный массив.

---
+++ seminar_8_1_two_dim_array/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 53: Задайте двумерный массив.
+//  Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] table = new int[5, 5];
+FillArray(table);
+Console.WriteLine("Исходный массив");
+PrintArray(table);
+int string1 = 0;
+int string2 = 4;
+Console.WriteLine("Замененный массив");
+StringReplace(string1, string2, table);
+PrintArray(table);
+
+// Замена строки массива
+void StringReplace(int string1Number, int string2Number, int[ , ] table)
+{
+int tmp = 0;
+    for (int columns = 0; columns < table.GetLength(1); columns++)
+    {
+        tmp = table[string1Number, columns];
+        table[string1Number, columns]=table[string2Number, columns];
+        table[string2Number, columns]= tmp;
+    }
+}
+// вывод массива
+void PrintArray(int[,] tab) 
+{
+for (int rows = 0; rows < tab.GetLength(0); rows++)
+{
+    Console.Write($"строка {rows} - ");
+    for (int columns = 0; columns < tab.GetLength(1); columns++)
+    {
+        Console.Write($" '{table[rows, columns]}'");
+    }
+Console.WriteLine();
+}
+}
+// Заполнение массива случайными числами
+void FillArray(int[,] tab)
+{
+for (int rows = 0; rows < tab.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < tab.GetLength(1); columns++)
+    {
+        tab[rows, columns] = new Random().Next(-10, 10);
+    }
+Console.WriteLine();
+}
+}
