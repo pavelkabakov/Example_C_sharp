89ccbd4285b95216c884b06bf9bfc368ecd59f24
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-23 21:57:30 +0300

    23/04/23

--- task_63_recursion/Program.cs
+++ task_63_recursion/Program.cs
@@ -2,6 +2,15 @@
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
 
-PrintArray(5);
+Console.WriteLine("Введите N:");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if (!isParsedN) { Console.WriteLine("Ошибка ввода, введено не число"); return; }
+Print(n);
+void Print(int n, int start = 1)
+{
+    if (start > n) { return; }
+    Console.WriteLine(start);
+    start++;
 
-int PrintArray(int)
+    Print(n, start);
+}
